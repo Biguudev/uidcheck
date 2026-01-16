2c0a4b647dd3624852441828d311030860300e58
B!guU <biguueditz@gmail.com>
2026-01-16 10:04:32 +0530

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,9 +1,29 @@
-js
-export default function handler(req, res) {
-  const { game_id, player_id } = req.body;
-  res.status(200).json({
-    message: 'Request received',
-    game_id,
-    player_id
-  });
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Only POST allowed");
+  }
+
+  try {
+    const body = await req.text();
+
+    const response = await fetch(
+      "https://www.novatopup.com/reseller/game/1",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Origin": "https://www.novatopup.com",
+          "Referer": "https://www.novatopup.com/reseller/game/1",
+          "User-Agent": "Mozilla/5.0"
+        },
+        body
+      }
+    );
+
+    const data = await response.text();
+    res.status(200).send(data);
+
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
 }
