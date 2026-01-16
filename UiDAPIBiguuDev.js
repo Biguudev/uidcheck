741bf5f742a9bcf12309c44aa2f8e0ee01b66820
B!guU <biguueditz@gmail.com>
2026-01-16 09:45:02 +0530

    Add files via upload

---
+++ UiDAPIBiguuDev.js
@@ -0,0 +1,16 @@
+const body = await req.text();
+
+  const response = await fetch("https://www.novatopup.com/reseller/game/1", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Origin": "https://www.novatopup.com",
+      "Referer": "https://www.novatopup.com/reseller/game/1",
+      "User-Agent": "Mozilla/5.0"
+    },
+    body
+  });
+
+  const data = await response.text();
+  res.status(200).send(data);
+}
